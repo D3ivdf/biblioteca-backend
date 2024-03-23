0ee11b70bfb687983cdb5f2e53802ec5ccf376b5
D3ivdf <dfdavidfernando@gmail.com>
2024-03-23 05:36:50 -0500

    ultimo comit

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('Nest biblioteca')
     .setDescription('Nest biblioteca API description')
